11275e16c76d9f7ef2b9096af92e00423f79cb5e
mrokk <matteorocco9@gmail.com>
2025-04-22 21:43:03 +0200

    added errors and defers

--- exercises/029_errdefer.zig
+++ exercises/029_errdefer.zig
@@ -32,13 +32,13 @@ fn makeNumber() MyErr!u32 {
 
     // Please make the "failed" message print ONLY if the makeNumber()
     // function exits with an error:
-    std.debug.print("failed!\n", .{});
+    errdefer std.debug.print("failed!\n", .{});
 
     var num = try getNumber(); // <-- This could fail!
 
     num = try increaseNumber(num); // <-- This could ALSO fail!
 
-    std.debug.print("got {}. ", .{num});
+    defer std.debug.print("got {}. ", .{num});
 
     return num;
 }
